2a820903ca6980e530b420ab3c49a27398acc707
HyeokE <jhjeong00@gmail.com>
2022-11-08 00:02:39 +0900

    feat: add reset.css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import Header from './components/Header';
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Main';
-import Error from './pages/Error';
+import Routes from './Routes';
+
 function App() {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="*" element={<Error />} />
-
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Home />} />
-        <Route path="/mypage" element={<Home />} />
-      </Routes>
+      <Routes />
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import './reset.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          flex: '1 0 auto',
+          minHeight: '100vh',
+        }}
+      >
+        <App />
+      </div>
     </BrowserRouter>
   </React.StrictMode>,
 );
