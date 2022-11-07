557208d5bec7093d5ecc508c867b2dca78a173a0
HyeokE <jhjeong00@gmail.com>
2022-11-08 00:21:08 +0900

    chore: eslint 편의성 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Header from './components/Header';
 import Routes from './Routes';
-import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/globalStyles';
 
 function App() {
