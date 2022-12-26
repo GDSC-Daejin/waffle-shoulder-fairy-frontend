0e23ab75a731f7e50a879e9110dbfbbbf6eb3a7e
Mini <ssh0040233@gmail.com>
2022-12-27 00:51:15 +0900

    edit modal delete

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,7 +4,6 @@ import TodoInsult from '../../components/Todo/TodoInsult';
 import TodoList from '../../components/Todo/TodoList';
 import { mockTodoList } from '../../mock/todo';
 //import { todoState } from '../../store/todoState';
-import ToDoEdit from '../../components/Todo/TodoEdit';
 import { getTodoList } from '../../apis/todo';
 import { getCategoryList } from '../../apis/category';
 
