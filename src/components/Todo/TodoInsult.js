b73e6a48fca9a6071df55fe4f37d1cdff2247392
HyeokE <jhjeong00@gmail.com>
2022-12-27 01:46:41 +0900

    chore: fix add api

--- src/components/Todo/TodoInsult.js
+++ src/components/Todo/TodoInsult.js
@@ -14,16 +14,20 @@ const categoryOptions = [
 const TodoInsult = ({ onInsert }) => {
   const [inputs, setInputs] = useState({
     content: '',
+    //TODO 카테고리 ID를 의미하는지 카테고리 텍스트를 의미하는건지 의미가 확실하지 않습니다.
     category: '',
   });
 
   const { addTodo } = todoState();
 
   const onChange = (e) => {
-    const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
+    //TODO e.target의 value는 selectBox에서의 name을 의미합니다.
+    // 각 데이터 마다 onChange함수를 따로 만들어보세요
+    const { value, name } = e.target;
+    console.log(e.target);
     setInputs({
-      ...inputs, // 기존의 input 객체를 복사한 뒤
-      [name]: value, // name 키를 가진 값을 value 로 설정
+      ...inputs,
+      [name]: value,
     });
   };
 
@@ -33,7 +37,9 @@ const TodoInsult = ({ onInsert }) => {
     console.log(inputs);
 
     if (inputs.content) {
-      onInsert(inputs.content, inputs.category);
+      //TODO 임시로 카테고리 아이디를 1로 설정해놓았습니다.
+      // onchange가 변경된 후 수정해주세요.
+      onInsert(inputs.content, '1');
       addTodo(inputs);
       setInputs({
         content: '',
